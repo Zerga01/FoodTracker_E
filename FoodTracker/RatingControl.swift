4f96fb3537d6998771149655b2b1a96441928030
Eliseev Viktor <v.s.eliseev@tinkoff.ru>
2021-02-16 18:25:35 +0300

    H/w 16.02

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,40 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by v.s.eliseev on 16.02.2021.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+//MARK: Initialization
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupButtons()
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+        setupButtons()
+    }
+}
+
+//MARK: private Methods
+
+private func setupButtons() {
+    
+    // Create the button
+    let button = UIButton()
+    button.backgroundColor = UIColor.red
+    
+    // Add constraints
+    button.translatesAutoresizingMaskIntoConstraints = false
+    button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
+    button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+    
+    // Add the button to the stack
+    addArrangedSubview(button)
+}
